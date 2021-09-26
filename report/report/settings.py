4403c7c900196baa4b91ed0e9490294213807665
khasbin <khasbin@gmail.com>
2021-09-26 23:55:29 +0545

    static file configured

--- report/report/settings.py
+++ report/report/settings.py
@@ -126,6 +126,12 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+
+    (BASE_DIR/'static'),
+    
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
